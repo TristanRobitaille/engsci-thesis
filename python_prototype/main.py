a550cde80562abb814f801cc995b15f5edabac4c
Tristan <robitaille.tristan@gmail.com>
2023-09-29 11:07:05 -0400

    Added input sequence truncate and padding mask

--- python_prototype/main.py
+++ python_prototype/main.py
@@ -8,17 +8,19 @@ import matplotlib.pyplot as plt
 
 #--- Notes ---#
 #Assume input data is a single sequential vector of voltages: x = [x0, x1, ... , xn] (can augment to multichannel later)
-#There are only a few sleep stage classes, and y = [y0, y1, ... , yn] is the sleep stage at each timestep
+#Assume x[0] is the oldest measurement.
+#There are only a few sleep stage classes, and y = [sleep stage for given clip]
 
 #--- Constants ---#
-WINDOW_LENGTH = 100 #Number of voltage samples (and corresponding sleep stage outputs)
-VOLTAGE_EMBEDDING_DEPTH = 25 #Length of voltage embedding vector for each timestep ('d')
+WINDOW_LENGTH = 7680 #Number of voltage samples @ 256Hz -> 30s clips
+VOLTAGE_EMBEDDING_DEPTH = 25 #Length of voltage embedding vector for each timestep
+
+PRINT_POSITION_EMBEDDING = False 
 
 #---Encoder---#
 #1) Voltage embedding
 
-#2) Positional encoding
-def positional_encoder(window_length=WINDOW_LENGTH, embed_depth=VOLTAGE_EMBEDDING_DEPTH):
+def positional_encoder(window_length:int=WINDOW_LENGTH, embed_depth:int=VOLTAGE_EMBEDDING_DEPTH) -> np.ndarray:
     """
     Return a matrix of dimensions WINDOW_LENGTH, VOLTAGE_EMBEDDING_DEPTH where each row represents
     the position encoding for a particular position in the input sequence voltage vector.
@@ -31,7 +33,42 @@ def positional_encoder(window_length=WINDOW_LENGTH, embed_depth=VOLTAGE_EMBEDDIN
     angle_matrix_rads[:, 0::2] = np.sin(angle_matrix_rads[:, 0::2])
     angle_matrix_rads[:, 1::2] = np.cos(angle_matrix_rads[:, 1::2])
 
+    angle_matrix_rads = tf.cast(angle_matrix_rads, dtype=tf.float16)
+    
+    if PRINT_POSITION_EMBEDDING:
+        plt.pcolormesh(angle_matrix_rads, cmap='RdBu')
+        plt.title('Position encoding matrix')
+        plt.xlabel('Position in voltage embedding')
+        plt.xlim((0, VOLTAGE_EMBEDDING_DEPTH))
+        plt.ylabel('Voltage position in input vector')
+        plt.colorbar()
+        plt.show()
+
     return angle_matrix_rads
+
+def truncate(input_sequence:tf.Tensor) -> tf.Tensor:
+    """
+    Truncate input sequence to (WINDOW_LENGTH) if it is too long. Return unchanged if not too long.
+    """
+
+    if input_sequence.shape[0] > WINDOW_LENGTH:
+        return input_sequence[-WINDOW_LENGTH-1:-1]
+    else:
+        return input_sequence
+
+
+def padding_mask(input:tf.Tensor) -> tf.Tensor:
+    """
+    Returns a mask to avoid attending to padded values (if input sequence is too short). 1 to attend, 0 to ignore.
+    We ignore values in the oldest positions (lower indices)
+    """
+
+    mask_keep = tf.constant(1, dtype=tf.int16, shape=(input.shape[0]))
+    
+    if input.shape[0] > WINDOW_LENGTH: #No need to pad
+        return mask_keep[0:WINDOW_LENGTH]
+    else:
+        return tf.concat([tf.constant(0, dtype=tf.int16, shape=(WINDOW_LENGTH-input.shape[0])), mask_keep], axis=0)
 
 #3) Multi-head attention
 
@@ -61,13 +98,8 @@ def positional_encoder(window_length=WINDOW_LENGTH, embed_depth=VOLTAGE_EMBEDDIN
 #7) Softmax
 
 def main():
-    pos_encoding = positional_encoder()
-    plt.pcolormesh(pos_encoding, cmap='RdBu')
-    plt.xlabel('Voltage position in input vector')
-    plt.xlim((0, VOLTAGE_EMBEDDING_DEPTH))
-    plt.ylabel('Position in voltage embedding')
-    plt.colorbar()
-    plt.show()
+    input = tf.constant(3, dtype=tf.int16, shape=(WINDOW_LENGTH-5))
+    print(padding_mask(input))
 
 if __name__ == "__main__":
     main()
