0f2f0af212ec161288529e8abe03490f555f602d
Tristan <robitaille.tristan@gmail.com>
2023-09-30 13:28:16 -0400

    Proper layer objects for Encoder and Decoder, and model for Transformer. Now shows non-zero trainable parameters

--- python_prototype/main.py
+++ python_prototype/main.py
@@ -20,7 +20,6 @@ OUTPUT_SEQUENCE_LENGTH = 1
 MAX_VOLTAGE = 2**16 #Maximum ADC code output
 NUM_SLEEP_STAGES = 5
 DATA_TYPE = tf.float32
-INITIAL_SLEEP_STAGE = 0
 
 VOLTAGE_EMBEDDING_DEPTH = 32 #Length of voltage embedding vector for each timestep. I let model dimensionality = embedding depth
 BATCH_SIZE = 16
@@ -46,7 +45,6 @@ def random_input_batch(length:int=CLIP_LENGTH) -> tf.Tensor:
     random_values = tf.random.normal((BATCH_SIZE, length), mean=0.0, stddev=1.0, dtype=tf.float32)
     return tf.math.abs(random_values)
 
-#--- Encoder ---#
 def positional_encoder(length:int=CLIP_LENGTH) -> tf.Tensor:
     """
     Return a matrix of dimensions (BATCH_SIZE, length, VOLTAGE_EMBEDDING_DEPTH) where each row represents
@@ -115,131 +113,132 @@ def self_attention(q:tf.Tensor, k:tf.Tensor, v:tf.Tensor, mask:tf.Tensor) -> (tf
 
     return self_attention, self_attention_weights
 
-def FullyConnected():
-    return tf.keras.Sequential([tf.keras.layers.Dense(FULLY_CONNECTED_DIM, activation='relu'), tf.keras.layers.Dense(VOLTAGE_EMBEDDING_DEPTH)])
+#--- Encoder ---#
+class Encoder(tf.keras.layers.Layer):
+    def __init__(self):
+        super().__init__()
+        self.positional_encoding = positional_encoder(OUTPUT_SEQUENCE_LENGTH)
+        
+        #Layers
+        self.voltage_embedding_layer = tf.keras.layers.Embedding(MAX_VOLTAGE, VOLTAGE_EMBEDDING_DEPTH)
+        self.dropout = tf.keras.layers.Dropout(rate=DROPOUT_RATE)
+        self.mha = tf.keras.layers.MultiHeadAttention(num_heads=MHA_NUM_HEADS, key_dim=VOLTAGE_EMBEDDING_DEPTH, dropout=DROPOUT_RATE)
+        self.norm = tf.keras.layers.LayerNormalization(epsilon=LAYER_NORM_EPSILON)
+        self.dense_relu = tf.keras.layers.Dense(FULLY_CONNECTED_DIM, activation='relu')
+        self.dense = tf.keras.layers.Dense(VOLTAGE_EMBEDDING_DEPTH)
 
-def encoder_layer(input:tf.Tensor, mask:tf.Tensor, training:bool=False) -> tf.keras.layers.Layer:
-    """
-    Single encoder layer
-    Dimensions:
-        input -> (BATCH_SIZE, CLIP_LENGTH, FULLY_CONNECTED_DIM)
-        mask -> (1, CLIP_LENGTH, 1)
-    """
-    #1 - Multi-head (self) attention
-    mha_output = tf.keras.layers.MultiHeadAttention(num_heads=MHA_NUM_HEADS, key_dim=VOLTAGE_EMBEDDING_DEPTH, dropout=DROPOUT_RATE)(input, input, input, mask)
+    def call(self, input:tf.Tensor, mask:tf.Tensor, training:bool=False):
+        #1 - Voltage embedding
+        voltage_embedding = self.voltage_embedding_layer(input)
+        voltage_embedding *= tf.math.sqrt(tf.cast(VOLTAGE_EMBEDDING_DEPTH, tf.float32))
 
-    #2 - Skip connection to Add & Norm
-    skip_mha = tf.keras.layers.LayerNormalization(epsilon=LAYER_NORM_EPSILON)(input + mha_output)
+        #2 - Positonal encoding
+        encoder_input = voltage_embedding + positional_encoder()
 
-    #3 - Feed MHA output to Fully Connected
-    fc_output = FullyConnected()(skip_mha)
+        #3 - [Training only] Apply dropout
+        encoder_input = self.dropout(encoder_input, training=training)
 
-    #4 - [Training only] Apply dropout to FC
-    fc_output = tf.keras.layers.Dropout(rate=DROPOUT_RATE)(fc_output, training=training)
-
-    #5 - Normalize output
-    return tf.keras.layers.LayerNormalization(epsilon=LAYER_NORM_EPSILON)(skip_mha + fc_output)
-
-def encoder_fwd_pass(input:tf.Tensor, mask:tf.Tensor, training:bool=False):
-    """
-    Performs one forward propagation through the encoder block
-    Dimensions:
-        -input: (BATCH_SIZE, CLIP_LENGTH)
-        -mask: (1, CLIP_LENGTH, 1)
-        -output: (BATCH_SIZE, CLIP_LENGTH, VOLTAGE_EMBEDDING_DEPTH)
-    """
-
-    #1 - Voltage embedding
-    voltage_embedding = tf.keras.layers.Embedding(MAX_VOLTAGE, VOLTAGE_EMBEDDING_DEPTH)(input)
-    voltage_embedding *= tf.math.sqrt(tf.cast(VOLTAGE_EMBEDDING_DEPTH, tf.float32))
-
-    #2 - Positonal encoding
-    encoder_input = voltage_embedding + positional_encoder()
-
-    #3 - [Training only] Apply dropout
-    encoder_input = tf.keras.layers.Dropout(rate=DROPOUT_RATE)(encoder_input, training=training)
-
-    #4 - Pass through encoder
-    return encoder_layer(encoder_input, mask, training)
+        #4 - Run through encoder
+        mha_output = self.mha(encoder_input, encoder_input, encoder_input, mask) #4.1 - Multi-head (self) attention
+        skip_mha = self.norm(encoder_input + mha_output) #4.2 - Skip connection to Add & Norm
+        fc_output = self.dense_relu(skip_mha) #4.3 - Feed MHA output to 2 Dense layers
+        fc_output = self.dense(fc_output)
+        fc_output = self.dropout(fc_output, training=training) #4.4 - [Training only] Apply dropout to FC
+        return self.norm(skip_mha + fc_output) #4.5 - Normalize output
 
 #--- Decoder ---#
-def decoder_layer(input:tf.Tensor, encoder_output:tf.Tensor, padding_mask:tf.Tensor, training:bool=False):
-    """
-    Single decoder layer
-    Dimensions:
-        input -> (BATCH_SIZE, CLIP_LENGTH, FULLY_CONNECTED_DIM)
-        encoder_output -> (BATCH_SIZE, CLIP_LENGTH, VOLTAGE_EMBEDDING_DEPTH)
-        padding_mask -> (1, OUTPUT_SEQUENCE_LENGTH, 1)
-    """
+class Decoder(tf.keras.layers.Layer):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.positional_encoding = positional_encoder(OUTPUT_SEQUENCE_LENGTH)
+        self.attention_weights = {}
 
-    #1 - Multi-head (self) attention
-    mha1_output, mha1_attention_weights = tf.keras.layers.MultiHeadAttention(num_heads=MHA_NUM_HEADS, key_dim=VOLTAGE_EMBEDDING_DEPTH, dropout=DROPOUT_RATE)(input, input, input, None, return_attention_scores=True)
+        #Layers
+        self.embedding_layer = tf.keras.layers.Embedding(NUM_SLEEP_STAGES, VOLTAGE_EMBEDDING_DEPTH)
+        self.dropout1 = tf.keras.layers.Dropout(rate=DROPOUT_RATE)
+        self.dropout2 = tf.keras.layers.Dropout(rate=DROPOUT_RATE)
+        self.mha1 = tf.keras.layers.MultiHeadAttention(num_heads=MHA_NUM_HEADS, key_dim=VOLTAGE_EMBEDDING_DEPTH, dropout=DROPOUT_RATE)
+        self.mha2 = tf.keras.layers.MultiHeadAttention(num_heads=MHA_NUM_HEADS, key_dim=VOLTAGE_EMBEDDING_DEPTH, dropout=DROPOUT_RATE)
+        self.norm1 = tf.keras.layers.LayerNormalization(epsilon=LAYER_NORM_EPSILON)
+        self.norm2 = tf.keras.layers.LayerNormalization(epsilon=LAYER_NORM_EPSILON)
+        self.norm3 = tf.keras.layers.LayerNormalization(epsilon=LAYER_NORM_EPSILON)
+        self.dense_relu = tf.keras.layers.Dense(FULLY_CONNECTED_DIM, activation='relu')
+        self.dense = tf.keras.layers.Dense(VOLTAGE_EMBEDDING_DEPTH)
 
-    #2 - Skip connection to Add & Norm
-    skip_mha1 = tf.keras.layers.LayerNormalization(epsilon=LAYER_NORM_EPSILON)(input + mha1_output)
+    def call(self, sleep_stage:tf.Tensor, encoder_output:tf.Tensor, padding_mask:tf.Tensor, training:bool=False):
+        #1 - Voltage embedding
+        embedding = self.embedding_layer(sleep_stage)
+        embedding *= tf.math.sqrt(tf.cast(VOLTAGE_EMBEDDING_DEPTH, tf.float32))
 
-    #3 - Multi-head attention with encoder output
-    mha2_output, mha2_attention_weights = tf.keras.layers.MultiHeadAttention(num_heads=MHA_NUM_HEADS, key_dim=VOLTAGE_EMBEDDING_DEPTH, dropout=DROPOUT_RATE)(skip_mha1, encoder_output, encoder_output, padding_mask, return_attention_scores=True)
+        #2 - Positonal encoding
+        decoder_input = embedding + self.positional_encoding
 
-    #4 - Skip connection to Add & Norm
-    skip_mha2 = tf.keras.layers.LayerNormalization(epsilon=LAYER_NORM_EPSILON)(skip_mha1 + mha2_output)
+        #3 - [Training only] Apply dropout
+        decoder_input = self.dropout1(decoder_input, training=training)
 
-    #5 - Feed MHA output to Fully Connected
-    fc_output = FullyConnected()(skip_mha2)
+        #4 - Go through decoder
+        mha1_output, mha1_attention_weights = self.mha1(decoder_input, decoder_input, decoder_input, None, return_attention_scores=True) #4.1 - Multi-head (self) attention
+        skip_mha1 = self.norm1(sleep_stage + mha1_output) #4.2 - Skip connection to Add & Norm
+        mha2_output, mha2_attention_weights = self.mha2(skip_mha1, encoder_output, encoder_output, padding_mask, return_attention_scores=True) #4.3 - Multi-head attention with encoder output
+        skip_mha2 = self.norm2(skip_mha1 + mha2_output) #4.4 - Skip connection to Add & Norm
+        fc_output = self.dense_relu(skip_mha2) #4.5 - Feed MHA output to 2 Dense layers
+        fc_output = self.dense(fc_output)
+        fc_output = self.dropout2(fc_output, training=training) #4.6 - [Training only] Apply dropout to FC
+        decoder_output = self.norm3(skip_mha2 + fc_output) #4.7 - Normalize output
 
-    #6 - [Training only] Apply dropout to FC
-    fc_output = tf.keras.layers.Dropout(rate=DROPOUT_RATE)(fc_output, training=training)
+        self.attention_weights[f'decoder_layer1_block1_self_att'] = mha1_attention_weights
+        self.attention_weights[f'decoder_layer1_block2_decenc_att'] = mha2_attention_weights
 
-    #7 - Normalize output
-    decoder_output = tf.keras.layers.LayerNormalization(epsilon=LAYER_NORM_EPSILON)(skip_mha2 + fc_output)
-
-    return decoder_output, mha1_attention_weights, mha2_attention_weights
-
-def decoder_fwd_pass(input:tf.Tensor, encoder_output:tf.Tensor, padding_mask:tf.Tensor, training:bool=False):
-    """
-    Performs one forward propagation through the encoder block
-    Dimensions:
-        input -> (BATCH_SIZE, CLIP_LENGTH, FULLY_CONNECTED_DIM)
-        encoder_output -> (BATCH_SIZE, CLIP_LENGTH, VOLTAGE_EMBEDDING_DEPTH)
-        padding_mask -> (1, OUTPUT_SEQUENCE_LENGTH, 1)
-    """
-    attention_weights = {}
-
-    #1 - Voltage embedding
-    embedding = tf.keras.layers.Embedding(NUM_SLEEP_STAGES, VOLTAGE_EMBEDDING_DEPTH)(input)
-    embedding *= tf.math.sqrt(tf.cast(VOLTAGE_EMBEDDING_DEPTH, tf.float32))
-
-    #2 - Positonal encoding
-    decoder_input = embedding + positional_encoder(OUTPUT_SEQUENCE_LENGTH)
-
-    #3 - [Training only] Apply dropout
-    decoder_input = tf.keras.layers.Dropout(rate=DROPOUT_RATE)(decoder_input, training=training)
-
-    #4 - Go through decoder
-    decoder_output, block1, block2 = decoder_layer(decoder_input, encoder_output, padding_mask, training=training)
-    attention_weights[f'decoder_layer1_block1_self_att'] = block1
-    attention_weights[f'decoder_layer1_block2_decenc_att'] = block2
-
-    return decoder_output, attention_weights
+        return decoder_output, self.attention_weights
 
 #--- Transformer ---#
-def transformer(eeg_clip:tf.Tensor, sleep_stage:tf.Tensor, encoder_padding_mask:tf.Tensor, decoder_padding_mask:tf.Tensor, training:bool=False):
-    #Go through encoder and decoder
-    encoder_output = encoder_fwd_pass(eeg_clip, encoder_padding_mask, training)
-    decoder_output, attention_weights = decoder_fwd_pass(sleep_stage, encoder_output, decoder_padding_mask, training)
+class Transformer(tf.keras.Model):
+    def __init__(self) -> None:
+        super().__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+        self.final_dense_layer = tf.keras.layers.Dense(NUM_SLEEP_STAGES, activation='softmax')
+    
+    def call(self, inputs, training:int=False):
+        eeg_clip, sleep_stage = inputs
 
-    #Final softmax
-    final_softmax = tf.keras.layers.Dense(NUM_SLEEP_STAGES, activation='softmax')(decoder_output)
+        #Masks
+        encoder_padding_mask = create_padding_mask(eeg_clip)
+        encoder_padding_mask = tf.reshape(encoder_padding_mask, (1, CLIP_LENGTH, 1))
+        decoder_padding_mask = tf.constant(True, shape=(1, OUTPUT_SEQUENCE_LENGTH, 1))
 
-    return final_softmax, attention_weights
+        encoder_output = self.encoder(eeg_clip, encoder_padding_mask, training)
+        sleep_stage, attention_weights = self.decoder(sleep_stage, encoder_output, padding_mask=decoder_padding_mask, training=training)
+
+        final_softmax = self.final_dense_layer(sleep_stage)
+
+        return final_softmax
+
+#--- Learning rate schedule ---#
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+  def __init__(self, warmup_steps=4000):
+    super().__init__()
+
+    self.d_model = tf.cast(VOLTAGE_EMBEDDING_DEPTH, tf.float32)
+    self.warmup_steps = warmup_steps
+
+  def __call__(self, step):
+    step = tf.cast(step, dtype=tf.float32)
+    arg1 = tf.math.rsqrt(step)
+    arg2 = step * (self.warmup_steps ** -1.5)
+
+    return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
 
 def main():
-    eeg_clip = random_input_batch()
-    padding_mask = create_padding_mask(eeg_clip)
-    padding_mask = tf.reshape(padding_mask, (1, CLIP_LENGTH, 1))
+    eeg_clip_batch = random_input_batch()
+    sleep_stage = tf.zeros((OUTPUT_SEQUENCE_LENGTH, 1))
 
-    sleep_stage_softmax, attention_weights = transformer(eeg_clip=eeg_clip, sleep_stage=tf.constant(INITIAL_SLEEP_STAGE, shape=(1)), 
-                                                        encoder_padding_mask=padding_mask, decoder_padding_mask=tf.constant(True, shape=(1, 1, 1)), training=False)
+    model = Transformer()
+    output = model((eeg_clip_batch, sleep_stage))
+    model.summary()
     
+    #Training
+    optimizer = tf.keras.optimizers.Adam(CustomSchedule(), beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+
 if __name__ == "__main__":
     main()
